e158843e797e3ecda6ba9854b576e8343ca5f5cd
josemar mendes <josemarmendes17@gmail.com>
2022-10-12 20:43:57 -0300

    implementação da listagem dos dados

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from .models import Transacao
 
 def home(request):
-    now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'contas/home.html')
+    data = {}
+    data['transacoes'] = ['t1','t2','t3']
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return  render(request, 'contas/listagem.html', data)
